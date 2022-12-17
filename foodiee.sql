42b15586dd137a81c06b93b24c5b7190eb803319
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-12-17 15:05:57 -0700

    Added the orders table called client_restaurant_order and the auxiliary table to store the items a order has

--- foodiee.sql
+++ foodiee.sql
@@ -46,6 +46,37 @@ LOCK TABLES `client` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `client_restaurant_order`
+--
+
+DROP TABLE IF EXISTS `client_restaurant_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client_restaurant_order` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `client_id` int(10) unsigned NOT NULL,
+  `restaurant_id` int(10) unsigned NOT NULL,
+  `is_confirmed` tinyint(1) NOT NULL DEFAULT 0,
+  `is_complete` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `client_restaurant_order_FK` (`client_id`),
+  KEY `client_restaurant_order_FK_1` (`restaurant_id`),
+  CONSTRAINT `client_restaurant_order_FK` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `client_restaurant_order_FK_1` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client_restaurant_order`
+--
+
+LOCK TABLES `client_restaurant_order` WRITE;
+/*!40000 ALTER TABLE `client_restaurant_order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client_restaurant_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `client_session`
 --
 
@@ -71,6 +102,91 @@ CREATE TABLE `client_session` (
 LOCK TABLES `client_session` WRITE;
 /*!40000 ALTER TABLE `client_session` DISABLE KEYS */;
 /*!40000 ALTER TABLE `client_session` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu_item`
+--
+
+DROP TABLE IF EXISTS `menu_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menu_item` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `restaurant_id` int(10) unsigned NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `description` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `price` float NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `menu_item_FK` (`restaurant_id`),
+  CONSTRAINT `menu_item_FK` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu_item`
+--
+
+LOCK TABLES `menu_item` WRITE;
+/*!40000 ALTER TABLE `menu_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu_item_images`
+--
+
+DROP TABLE IF EXISTS `menu_item_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menu_item_images` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `menu_item_id` int(10) unsigned NOT NULL,
+  `file_name` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `description` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `menu_item_images_FK` (`menu_item_id`),
+  CONSTRAINT `menu_item_images_FK` FOREIGN KEY (`menu_item_id`) REFERENCES `menu_item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu_item_images`
+--
+
+LOCK TABLES `menu_item_images` WRITE;
+/*!40000 ALTER TABLE `menu_item_images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu_item_images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu_item_order`
+--
+
+DROP TABLE IF EXISTS `menu_item_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menu_item_order` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` int(10) unsigned NOT NULL,
+  `menu_item_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `menu_item_order_FK` (`order_id`),
+  KEY `menu_item_order_FK_1` (`menu_item_id`),
+  CONSTRAINT `menu_item_order_FK` FOREIGN KEY (`order_id`) REFERENCES `client_restaurant_order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `menu_item_order_FK_1` FOREIGN KEY (`menu_item_id`) REFERENCES `menu_item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu_item_order`
+--
+
+LOCK TABLES `menu_item_order` WRITE;
+/*!40000 ALTER TABLE `menu_item_order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu_item_order` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -175,4 +291,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-17 14:29:15
+-- Dump completed on 2022-12-17 15:04:56
