556d948ba0073e0c36fa7aee70075cedd0a71dcc
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-12-23 12:07:16 -0700

    Fixing the post function for menu item images to know send a token and handle token errors

--- endpoints/menu_item_images.py
+++ endpoints/menu_item_images.py
@@ -21,11 +21,12 @@ def post():
     if(filename == None):
         return make_response(json.dumps("Sorry, an error has occurred", default=str), 500)
 
-    results = run_statement('CALL add_menu_item_image(?,?,?)', [request.form.get('menu_item_id'), filename,
-    request.form['description']])
+    results = run_statement('CALL add_menu_item_image(?,?,?,?)', [request.form.get('menu_item_id'), filename, request.form['description'], request.headers.get('token')])
 
     if(type(results) == list and len(results) != 0):
         return make_response(json.dumps(results[0], default=str), 200)
+    elif(type(results) == list and len(results) == 0):
+        return make_response(json.dumps("Wrong token.", default=str), 400)
     elif(results.startswith("Duplicate entry")):
         image_path = os.path.join('restaurant_menu_images', filename)
         os.remove(image_path)
