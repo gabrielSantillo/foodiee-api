b9f2db326982134f102ce929eff6f227e4c6f76f
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-12-30 17:13:50 -0700

    Update the client post function to not have a username data required

--- endpoints/client.py
+++ endpoints/client.py
@@ -6,7 +6,7 @@ import json
 from dbhelpers import run_statement
 
 def post():
-    is_valid = check_endpoint_info(request.json, ['email', 'first_name','last_name', 'username', 'password'])
+    is_valid = check_endpoint_info(request.json, ['email', 'first_name','last_name', 'password'])
     if(is_valid != None):
         return make_response(json.dumps(is_valid, default=str), 400)
 
@@ -14,7 +14,7 @@ def post():
     salt = uuid4().hex
 
     results = run_statement('CALL add_client(?,?,?,?,?,?,?)', [request.json.get('email'), request.json.get('first_name'),               
-    request.json.get('last_name'), request.json.get('username'), request.json.get('password'), salt, token])
+    request.json.get('last_name'), request.json.get('password'), salt, token])
 
     if(type(results) == list and len(results) != 0):
         return make_response(json.dumps(results[0], default=str), 200)
