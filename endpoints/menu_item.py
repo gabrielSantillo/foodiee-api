a6f41a7119f7f57019cf3b928f24ae641c929782
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-03 11:15:16 -0700

    Changing the the menu item get request to call another procedure

--- endpoints/menu_item.py
+++ endpoints/menu_item.py
@@ -25,21 +25,17 @@ def post():
 
 
 def get():
-    is_valid_header = check_endpoint_info(request.headers, ['token'])
-    if(is_valid_header != None):
-        return make_response(json.dumps(is_valid_header, default=str), 400)
-
     is_valid = check_endpoint_info(request.args, ['restaurant_id'])
     if(is_valid != None):
         return make_response(json.dumps(is_valid, default=str), 400)
 
-    results = run_statement('CALL get_all_menu_items(?,?)', 
+    results = run_statement('CALL get_menu_items_by_restaurant_id(?)', 
     [request.args.get('restaurant_id'), request.headers.get('token')])
 
     if(type(results) == list and len(results) != 0):
         return make_response(json.dumps(results, default=str), 200)
     elif(type(results) == list and len(results) == 0):
-        return make_response(json.dumps("Wrong token and/or restaurant id.", default=str), 
+        return make_response(json.dumps("Wrong restaurant id.", default=str), 
         400)
     else:
         return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
